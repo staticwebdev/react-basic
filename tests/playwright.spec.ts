3b82aa5479437db81016592d142d761e2ae6fdb7
Hannah Zhu <t-hannzhu@microsoft.com>
2022-06-14 16:03:14 -0700

    removed commented test code

--- tests/playwright.spec.ts
+++ tests/playwright.spec.ts
@@ -3,12 +3,4 @@ import { test, expect } from '@playwright/test';
 test('basic test', async ({ page }) => {
   await page.goto('/');
   await expect(page.locator('div div')).toContainText('Hello World');
-  // const divs = page.locator('div');
-  // var found = false
-  // for(var d in divs){
-  //     if(d 'Hello World'){
-  //       found = true
-  //     }
-  // }
-  // await expect(found).toEqual(true);
 })
